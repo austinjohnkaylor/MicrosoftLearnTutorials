c5161a3eb778535417b282103f30d910397b9e90
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-13 13:35:56 -0400

    Adding a custom identity user class

---
+++ Secure a .NET web app with the ASP.NET Core Identity framework/API/Data/CustomIdentityUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API.Data;
+
+public class CustomIdentityUser : IdentityUser
+{
+    [ProtectedPersonalData] 
+    public string FirstName { get; set; }
+    
+    [ProtectedPersonalData] 
+    public char MiddleInitial { get; set; }
+    
+    [ProtectedPersonalData] 
+    public string LastName { get; set; }
+    
+    [ProtectedPersonalData]
+    public DateTime BirthDate { get; set; }
+    
+    [ProtectedPersonalData]
+    public Address Address { get; set; }
+}
