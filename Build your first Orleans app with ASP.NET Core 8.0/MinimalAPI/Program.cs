1bd69c7ce861ba4a19a89662fda06950dd3c215a
Austin Kaylor <AustinJKaylor@eaton.com>
2024-07-08 15:59:53 -0400

    Enabling the MinimalWebAPI to use Orleans with Localhost clustering

--- Build your first Orleans app with ASP.NET Core 8.0/MinimalAPI/Program.cs
+++ Build your first Orleans app with ASP.NET Core 8.0/MinimalAPI/Program.cs
@@ -1,11 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Host.UseOrleans(siloBuilder =>
+{
+    siloBuilder.UseLocalhostClustering();
+});
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
