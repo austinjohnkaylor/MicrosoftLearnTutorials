98f6f387973b1cac8ddc4740782cc84c6d84e739
Austin Kaylor <AustinJKaylor@eaton.com>
2024-07-08 15:58:34 -0400

    Creating an interface and concrete class for the UrlShortener grain

---
+++ Build your first Orleans app with ASP.NET Core 8.0/MinimalAPI/IUrlShortenerGrain.cs
@@ -0,0 +1,8 @@
+﻿namespace MinimalAPI;
+
+public interface IUrlShortenerGrain : IGrainWithStringKey
+{
+    Task SetUrl(string fullUrl);
+
+    Task<string> GetUrl();
+}
---
+++ Build your first Orleans app with ASP.NET Core 8.0/MinimalAPI/UrlShortenerGrain.cs
@@ -0,0 +1,21 @@
+﻿namespace MinimalAPI;
+
+/// <summary>
+/// A <see cref="Grain"/> that stores a shortened URL
+/// </summary>
+public sealed class UrlShortenerGrain : Grain, IUrlShortenerGrain
+{
+    private KeyValuePair<string, string> _cache;
+
+    public Task SetUrl(string fullUrl)
+    {
+        _cache = new(
+            key: this.GetPrimaryKeyString(),
+            value: fullUrl);
+
+        return Task.CompletedTask;
+    }
+
+    public Task<string> GetUrl() =>
+        Task.FromResult(_cache.Value);
+}
