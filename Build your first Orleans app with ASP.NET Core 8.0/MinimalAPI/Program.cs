6f5892344d6f36f6f07256c2c3e892ca69316dd5
Austin Kaylor <AustinJKaylor@eaton.com>
2024-07-08 16:12:06 -0400

    Adding some endpoints for shortening and going to the shortened URL

--- Build your first Orleans app with ASP.NET Core 8.0/MinimalAPI/Program.cs
+++ Build your first Orleans app with ASP.NET Core 8.0/MinimalAPI/Program.cs
@@ -1,3 +1,5 @@
+using MinimalAPI;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,29 +23,62 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+// This endpoint does the following:
+// 1. Ensures that the URL query string is well-formed and not empty.
+// 2. A unique, short ID is created using the Guid class.
+// 3. The IUrlShortenerGrain is retrieved for the shortened route segment.
+// 4. The full URL is stored in the grain.
+// 5. The shortened URL is returned to the user on the /go rout
+app.MapGet("/shorten",
+    static async (IGrainFactory grains, HttpRequest request, string url) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        var host = $"{request.Scheme}://{request.Host.Value}";
+
+        // Validate the URL query string.
+        if (string.IsNullOrWhiteSpace(url) &&
+            Uri.IsWellFormedUriString(url, UriKind.Absolute) is false)
+        {
+            return Results.BadRequest($"""
+                                       The URL query string is required and needs to be well formed.
+                                       Consider, ${host}/shorten?url=https://www.microsoft.com.
+                                       """);
+        }
+
+        // Create a unique, short ID
+        var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+
+        // Create and persist a grain with the shortened ID and full URL
+        IUrlShortenerGrain? shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        await shortenerGrain.SetUrl(url);
+
+        // Return the shortened URL for later use
+        UriBuilder resultBuilder = new(host)
+        {
+            Path = $"/go/{shortenedRouteSegment}"
+        };
+
+        return Results.Ok(resultBuilder.Uri);
+    });
+
+// This endpoint does the following:
+// 1. The shortened route segment is used to retrieve the corresponding IUrlShortenerGrain instance.
+// 2. The full URL is retrieved from the grain.
+// 3. The full URL is used to redirect the user to the correct location
+app.MapGet("/go/{shortenedRouteSegment:required}",
+    static async (IGrainFactory grains, string shortenedRouteSegment) =>
+    {
+        // Retrieve the grain using the shortened ID and url to the original URL
+        var shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        var url = await shortenerGrain.GetUrl();
+
+        // Handles missing schemes, defaults to "http://".
+        var redirectBuilder = new UriBuilder(url);
+
+        return Results.Redirect(redirectBuilder.Uri.ToString());
+    });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
